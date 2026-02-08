454050294f7a5355ab46fe9a58f789f9303c18ef
Jay <jay@example.com>
2026-02-08 11:53:33 +0800

    Fix: Destructure MongoStore from connect-mongo exports

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // =====================
 // Session (before Passport)
 // =====================
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 
 
 console.log('🔍 DEBUG MongoStore type:', typeof MongoStore);
